cf754db202d5bc454caf64efba9755089e3db912
Henrik Voss <henrik@comoyo.com>
2013-04-20 22:42:55 +0200

    Testing

--- index.php
+++ index.php
@@ -34,6 +34,7 @@
 		<div class="content">
 			
 			<p>Aenean eleifend, orci eu semper cursus, neque magna ultrices lacus, ac cursus nisl diam ac metus. Morbi ut ligula magna, quis placerat nisl. Morbi euismod, eros in sollicitudin porta, ligula tellus rhoncus mi, quis tempus sapien dui sed ante. Ut faucibus fringilla ante, vel elementum lectus cursus ut. Aliquam egestas sagittis libero, vulputate tempus urna malesuada eget. Donec neque orci, porta sit amet porttitor ut, dictum eget nisi. Maecenas nec lorem nulla. Fusce eget risus leo. In congue pulvinar nisi, et interdum augue iaculis sit amet. Vivamus adipiscing iaculis egestas.</p>
+			<p>Curabitur vehicula condimentum turpis at luctus. Vestibulum eget nunc libero. Sed consequat luctus massa, ac tempus elit pretium quis. Nunc vel posuere libero. Fusce commodo cursus erat quis laoreet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin vestibulum, odio nec pharetra aliquam, elit nibh fringilla erat, et pretium lacus erat in mauris. Mauris at felis odio. Morbi neque magna, condimentum sed ullamcorper eu, rutrum ut arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel elementum purus.</p>
 			
 		</div>
 		
