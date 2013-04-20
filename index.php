4e741734c00eca90a4fa6853e128b2f714b75a3e
Henrik Voss <henrik@comoyo.com>
2013-04-20 21:58:39 +0200

    Added a few pages
    Added a CSS file
    made menu.html

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 <html lang="en">
 	
-	<head fontsize=45>
+	<head>
 		
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-		
+		<link rel="stylesheet" type="text/css" href="main.css" />
 		
 		
 		<title>Danseskolen</title>
@@ -13,9 +13,32 @@
 	</head>
 	
 	<body>
-	
+		<div class="container">
+
+		<div class="header">
+			
+			<h1>Danskeskolen</h1>
+			
+		</div>
+		
+		<div class="menu">
+			
+			<?php
+			
+				include "menu.html";
+			
+			?>
+			
+		</div>
+		
+		<div class="content">
+			
+			<p>Aenean eleifend, orci eu semper cursus, neque magna ultrices lacus, ac cursus nisl diam ac metus. Morbi ut ligula magna, quis placerat nisl. Morbi euismod, eros in sollicitudin porta, ligula tellus rhoncus mi, quis tempus sapien dui sed ante. Ut faucibus fringilla ante, vel elementum lectus cursus ut. Aliquam egestas sagittis libero, vulputate tempus urna malesuada eget. Donec neque orci, porta sit amet porttitor ut, dictum eget nisi. Maecenas nec lorem nulla. Fusce eget risus leo. In congue pulvinar nisi, et interdum augue iaculis sit amet. Vivamus adipiscing iaculis egestas.</p>
+			
+		</div>
 		
 		
+		</div>
 	</body>
 
 </html>
