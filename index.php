f7880d92ab998b23fc5abe629410565afe77fc68
Henrik Voss <henrik@comoyo.com>
2013-04-20 21:30:04 +0200

    Added index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+	
+	<head>
+		
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		
+		
+		
+		<title>Danseskolen</title>
+	
+	</head>
+	
+	<body>
+		
+		
+		
+	</body>
+
+</html>
